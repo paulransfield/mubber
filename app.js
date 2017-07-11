62a4d48b0456ed70ec4a645b9c1299ccde50857f
Paul <paulr@freelearn.tv>
2017-07-12 06:54:04 +1200

    Add get route handler in express web server

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+
+//express webserver request route handlers
+
+app.get('/api', (req, res) => {
+  res.send( { hi: "there"});
+});
+
+module.exports = app;
